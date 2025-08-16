72fb6e5441d0987fe62e0622d58ede8ba084ac92
marcsman007 <macaburasmarcjayson@gmail.com>
2025-08-16 20:21:18 +0800

    Added .env in frontend and updated api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,32 +1,28 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000';
+// Use environment variable for backend URL
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-export const getBackendMessage = async () => {
-  const res = await axios.get(`${BASE_URL}/`);
-  return res.data;
-};
-
-// Fetch all users
+// Get all users
 export const getUsers = async () => {
   const res = await axios.get(`${BASE_URL}/users`);
   return res.data;
 };
 
-// Add new user
+// Create a new user
 export const createUser = async (userData) => {
   const res = await axios.post(`${BASE_URL}/users`, userData);
-  return res.data;
-};
-
-// Delete user
-export const deleteUser = async (id) => {
-  const res = await axios.delete(`${BASE_URL}/users/${id}`);
   return res.data;
 };
 
 // Update user
 export const updateUser = async (id, updatedData) => {
   const res = await axios.put(`${BASE_URL}/users/${id}`, updatedData);
+  return res.data;
+};
+
+// Delete user
+export const deleteUser = async (id) => {
+  const res = await axios.delete(`${BASE_URL}/users/${id}`);
   return res.data;
 };
